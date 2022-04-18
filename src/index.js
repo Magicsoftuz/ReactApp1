5c79e08e868c52e22af2cb3b4fd841a15f0233eb
MagicSoft <magicsoft.uz@gmail.com>
2022-04-18 11:42:55 +0500

    Add form JSX format

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import ReactDOM from "react-dom";
 // Component yaratish kerak
 
 const App = function () {
-  return <div>Hello World</div>;
+  return (
+    <div style={{ fontSize: "30px" }}>
+      <label className="labelName" htmlFor="name" id="name">
+        Enter Name:
+      </label>
+      <input id="name" />
+      <button type="submit" style={{ color: "white", backgroundColor: "blue" }}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
